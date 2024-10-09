ba9ccdfee14d4b9cf99df532dce450dbe1a4a2a6
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-09 08:34:52 -0300

    adicionando particularidades no js

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,22 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+// Declara uma variável
+//let nomeDaVariável = valor;
+  let nomeDaVariavel  = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um comentário
+
+/*
+  Este é
+  um comentário
+  de
+  múltiplas linhas
+*/
