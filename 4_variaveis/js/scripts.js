5a282833a62c1b9ba08eb94d8d55f24335b4dc89
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-10 10:44:24 -0300

    variaveis

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,44 @@
+let teste = 1;
+
+let nome = "Luiz";
+
+console.log(teste);
+
+teste = 'Primer';
+
+console.log(teste);
+
+console.log(nome);
+
+let $nome = 'ASD';
+let _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+let nome5 = 'ASD3';
+
+console.log(nome5);
+
+// camelCase
+let meuPrimeiroNome = 'jeferson';
+
+console.log(meuPrimeiroNome); 
+
+let meusobrenome = "primer";
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+let meuNome;
+
+console.log(meuNome);
+
+meuNome = "jeferson";
+
+console.log(meuNome);
