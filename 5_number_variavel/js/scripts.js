0a81eb0ae137387040319a03d40b8d0809bc0237
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-11 12:36:45 -0300

    Number Variaveis

---
+++ 5_number_variavel/js/scripts.js
@@ -0,0 +1,23 @@
+let numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+let float = 5.32;
+
+console.log(float);
+console.log(typeof float);
+
+let textoComNumero = '523';
+let textoComNumero1 = "523";
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+console.log(textoComNumero1);
+console.log(typeof textoComNumero1);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
