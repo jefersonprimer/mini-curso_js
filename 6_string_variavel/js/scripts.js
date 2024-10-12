5d275e1ed048b9d2fa955c5a88db8c1da9f1ebf0
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-12 07:14:46 -0300

    String variaveis

---
+++ 6_string_variavel/js/scripts.js
@@ -0,0 +1,26 @@
+let nome = "Jeferson";
+let sobrenome = "Primer";
+
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "abc");
+
+console.log(nomeCompleto);
+
+console.log(typeof "6.6666");
+
+let frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse: "Olá"');
+
+document.write("Ele disse: 'Olá'");
+
+console.log("Este número: " + nome);
