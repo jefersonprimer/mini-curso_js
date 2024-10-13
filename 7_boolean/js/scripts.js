a234b77b1b8c121d7425f2ccaedb907f11213ddd
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-13 15:38:41 -0300

    boolean

---
+++ 7_boolean/js/scripts.js
@@ -0,0 +1,12 @@
+let verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+let falso = false;
+
+console.log(falso);
+console.log(typeof falso);
