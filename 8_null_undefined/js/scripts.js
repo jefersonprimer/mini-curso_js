e347780bd32c020b419e7430ada3d778d05e6b7d
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-14 07:49:07 -0300

    adicionando Null e undefined

---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,15 @@
+// Hoisting, içamento
+console.log(sobrenome);
+console.log(numero);
+
+let nome = null;
+let sobrenome = "Primer";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Jeferson";
+
+console.log(nome);
+
+let numero = 5;
