1e4af3137b53b0b28d67d3b6505c1fffdc5d2986
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-16 08:31:27 -0300

    adicionando arrays em js

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,18 @@
+let list = [5, "Matheus", true, {teste: 1, teste: 2}]; // obj => {}
+
+console.log(list);
+
+let numbers = [2,3,4,5,6];
+
+console.log(numbers);
+
+console.log(list[1]);
+console.log(numbers[0]);
+
+list[4] = 10;
+
+list[0] = "Teste";
+
+console.log(list);
+
+console.log(typeof list);
