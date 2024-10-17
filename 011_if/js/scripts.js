be05e2d971040dd71ac26d467254d536c46d9091
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-17 07:03:28 -0300

    adicionando o operador condicional If

---
+++ 011_if/js/scripts.js
@@ -0,0 +1,14 @@
+let idade = 16;
+let idadeMinima = 18;
+
+console.log("Antes do if");
+
+if(idade > idadeMinima) {
+  console.log("Pode fazer a carteira de habilitação");
+}
+
+if(idade > 15) {
+  console.log("Precisa esperar 3 anos ainda");
+}
+
+console.log("Depois do if");
