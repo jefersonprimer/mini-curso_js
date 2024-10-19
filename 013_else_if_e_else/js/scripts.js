2252967ec7d812643c26d780ba6b8a103b1baa68
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-19 06:08:52 -0300

    adicionando else if, else

---
+++ 013_else_if_e_else/js/scripts.js
@@ -0,0 +1,25 @@
+let nome = "Xavier";
+
+if(nome == "Pedro") {
+  console.log("O nome dele é Pedro");
+} else if(nome == "Jeferson") {
+  console.log("O nome é jeferson");
+} else if(nome == "Xavier") {
+  console.log("O nome é Xavier");
+} else {
+  console.log("Ele possui outro nome!");
+}
+
+let idade = 19;
+
+if(idade > 20) {
+  console.log("Ele pode entrar na festa!");
+} else if(idade >= 18) {
+  console.log("Ele só pode entrar com autorização");
+}
+
+if(nome == "Jeferson") {
+  console.log("teste");
+} else {
+  console.log("testando");
+}
