e649b1269e27c1cf13a28fa009ae68a45ec99ffb
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-21 06:53:31 -0300

    adicionando operador logico and

---
+++ 015_operador_logico_and/js/scripts.js
@@ -0,0 +1,18 @@
+let idade = 16;
+let nome = "João";
+
+if(nome == "João" && idade == 16) {
+  console.log("O João pode entrar na aula de esgrima");
+} else {
+  console.log("Este não é o João");
+}
+
+if(1 == 1 && 3 > 2 && true) {
+  console.log("Passou");
+}
+
+if((1 == 1 && 3 > 3) && true) {
+  console.log("Passou");
+} else if(nome === "João" && idade >= 14) {
+  console.log("Aqui passa!");
+}
