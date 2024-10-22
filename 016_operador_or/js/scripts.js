cea64a60786fe08ba9c2fe44b30996231d5648dd
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-22 06:07:19 -0300

    adicionando operador or

---
+++ 016_operador_or/js/scripts.js
@@ -0,0 +1,14 @@
+let idade = 12;
+let nome = "João";
+
+if(nome == "João" || idade > 14) {
+  console.log("Pode entrar na aula de esgrima");
+} else {
+  console.log("Não pode entrar");
+}
+
+if(nome == "Pedro" && (30 > 20 || 10 == 10)) {
+  console.log("testando");
+} else {
+  console.log("não entrou");
+}
