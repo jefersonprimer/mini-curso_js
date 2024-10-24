894aab97dd97a3216a357a824ec17e37b8a702d8
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-24 06:59:55 -0300

    add while loop

---
+++ 018_while/js/scripts.js
@@ -0,0 +1,36 @@
+// while loop precisa de um contador, uma condição e um incremetador
+
+let x = 0; // contador
+
+
+//   condição
+while(x <= 5) {
+
+  console.log("Testando repetição " + x);
+
+  // incremetador
+  x++;
+
+}
+
+let arr = ['teste', 'testando', 'a', 'b'];
+let y = 0;
+
+while(y <= 3) {
+
+  console.log(arr[y]);
+
+  y++;
+
+}
+
+let palavra = "Jeferson";
+let i = 0;
+
+while(i <= 6) {
+
+  console.log(palavra[i]);
+
+  i += 1;
+
+}
