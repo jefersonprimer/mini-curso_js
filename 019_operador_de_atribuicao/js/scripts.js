09db2e6419916f62587d2831847f5695b6d9d8c2
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-25 06:11:23 -0300

    add operador de atribuicao

---
+++ 019_operador_de_atribuicao/js/scripts.js
@@ -0,0 +1,39 @@
+let x = 1;
+let y = 2;
+
+// soma
+console.log(x = x + y);
+console.log(x += y);
+
+// subtracao
+console.log(x -= y);
+
+// multiplicação
+console.log(x *= y);
+
+// divisão
+console.log(x /= y);
+
+// loops
+console.log(x++);
+console.log(x--);
+
+while(x <= 100) {
+
+  console.log(x);
+
+  x *= 2;
+
+}
+
+console.log(x);
+
+let j = 5;
+
+while(x > 0) {
+
+  console.log(x);
+
+  x -= j;
+
+}
