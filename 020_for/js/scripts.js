f1bb45a6ea8d6c42129f354484db449982fdc823
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-26 16:58:32 -0300

    add loop for

---
+++ 020_for/js/scripts.js
@@ -0,0 +1,18 @@
+// contador, condicao, incrementador 
+for(let i = 0; i < 10; i++) {
+
+  console.log("Repetindo for: " + i);
+
+}
+
+let arr = [1,2,3,4];
+
+for(let j = 0; j < arr.length; j++) {
+  console.log(arr[j]);
+}
+
+console.log(arr.length);
+
+for(let x = 5; x < 100; x *= 3) {
+  console.log(x);
+}
