932f5fe5df9f2c2998d7b391c4a45ec608b2b256
jefersonprimer <jefersonpriimer@gmail.com>
2024-10-30 06:54:47 -0300

    add melhorias

---
+++ 024_escopo_js/js/scripts.js
@@ -0,0 +1,33 @@
+let x = 1;
+
+let y = 3;
+
+console.log(x, y);
+
+function teste() {
+
+  let z = 0;
+
+  console.log(z);
+
+}
+
+teste();
+
+function testando() {
+
+  let z = 5;
+
+  console.log(z);
+
+}
+
+testando();
+
+if(true) {
+  
+  let p = 1;
+
+}
+
+console.log(p);
