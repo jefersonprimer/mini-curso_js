7a98e378e12c09cf0f127f5ae1714318f6f52626
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-02 07:55:25 -0300

    add metodos string

---
+++ 027_metodos_de_string/js/scripts.js
@@ -0,0 +1,29 @@
+// length
+
+let nome = "Jeferson";
+
+console.log(nome.length);
+
+let obj = "bola";
+
+console.log(obj.length);
+
+// indexOf
+
+console.log(nome[2]);
+
+let frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice
+
+let roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+// replace
+
+let novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
