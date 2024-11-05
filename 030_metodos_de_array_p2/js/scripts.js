bf96a05806461885529c92de31f67f8e9e4876d2
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-05 06:23:03 -0300

    add metodos array p2

---
+++ 030_metodos_de_array_p2/js/scripts.js
@@ -0,0 +1,24 @@
+// splice
+let arr = [1,2,3,4,5];
+
+arr.splice(2, 0, 999);
+
+console.log(arr);
+
+arr.splice(4, 1);
+
+console.log(arr);
+
+// indexOf
+
+console.log(arr.indexOf(5));
+
+
+// join
+let arr2 = ["O", "rato", "roeu", "a", "roupa"];
+
+console.log(arr2.join(","));
+
+
+// reverse
+console.log(arr2.reverse());
