41f807a4a6e090c3e8f4fad00046f56230651aab
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-08 06:04:16 -0300

    acessando o DOM

---
+++ 033_acessando_dom/js/scripts.js
@@ -0,0 +1,18 @@
+// tag
+let titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo);
+
+let lis = document.getElementsByTagName('li');
+
+console.log(lis);
+
+// id
+let paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+// class
+let itensDaLista = document.getElementsByClassName('item');
+
+console.log(itensDaLista);
