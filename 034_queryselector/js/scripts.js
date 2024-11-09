a41525301dee11a2f873cb9457db4693ea863fcf
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-09 08:24:16 -0300

    queryselector, manipulacao DOM

---
+++ 034_queryselector/js/scripts.js
@@ -0,0 +1,25 @@
+const itensClasse = document.getElementsByClassName('item');
+
+console.log(itensClasse);
+
+// querySelectorAll
+const itensQuery = document.querySelectorAll('#lista2 li');
+
+console.log(itensQuery);
+
+const itensQuery2 = document.querySelectorAll('#lista .item');
+
+console.log(itensQuery2);
+
+// querySelector
+const lista = document.querySelector('#lista');
+
+console.log(lista);
+
+const primeiraLista = document.querySelector('ul');
+
+console.log(primeiraLista);
+
+const span = document.querySelector('#paragrafo span');
+
+console.log(span);
