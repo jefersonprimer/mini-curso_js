c6cfd17e981957c03296cb3514776dac4d43a5d2
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-12 06:30:38 -0300

    feat: add como remover elementos da DOM

---
+++ 037_remover_elementos/js/scripts.js
@@ -0,0 +1,14 @@
+// removendo elemento filho
+
+const container = document.querySelector("#container");
+
+const p = document.querySelector("#container p");
+
+container.removeChild(p);
+
+
+// remover o elemento
+
+const subtitle = document.querySelector(".subtitle");
+
+subtitle.remove();
